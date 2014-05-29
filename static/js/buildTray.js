51f8f9fc11b5fa3e2b5f6531513f2c820c1bd5d2
Sam Agnew <sagnew@eden.rutgers.edu>
2014-05-29 02:12:09 -0400

    Display menus from clicked restaurants

--- static/js/buildTray.js
+++ static/js/buildTray.js
@@ -1,31 +1,43 @@
 $(document).ready(function () {
     $('.restaurant').click(function () {
+        $('#menu').html('<h2 align="center">Menu</h2>');
         var index = $(this).attr('id');
         var rid = restaurants[index].id;
         $.getJSON('/fee', { 'rid': rid, 'addr': addr, 'city': city, 'zip': zip }, function (feeData) {
             var minOrderAmount = parseFloat(feeData.mino);
             var availableMeals = feeData.meals;
             var willStillDeliver = feeData.delivery;
-            if (willStillDeliver === '0'){
-                // Display a notifation that this restaurant is no longer delivering
-            }
-            else{
+            if (willStillDeliver === '0' || feeData.msg){
+                $('#menu').append('<h3 align="center">This restaurant is no longer currently delivering</h3>');
+            } else {
                 $.getJSON('/menuitems', { 'rid': rid }, function (menu) {
-                    var displayItems = [];
+                    // Intentionally global for now
+                    displayItems = [];
+                    // Iterate through the data to build an array of all valid menu items
                     for (var i = 0; i < menu.length; i += 1) {
                         for (var j = 0; j < menu[i].children.length; j += 1) {
                             // i is the index in the menu, and j is the index
                             // for the menu's children
                             var item = menu[i].children[j];
-                            if (item.availability === 0) {
-                                displayItems.push({
-                                    'name': item.name,
-                                    'description': item.descrip,
-                                    'price': item.price
-                                });
+                            for (var h =0; h < item.availability.length; h += 1) {
+                                if (availableMeals.indexOf(item.availability[h]) !== -1) {
+                                    displayItems.push({
+                                        'name': item.name,
+                                        'description': item.descrip,
+                                        'price': item.price
+                                    });
+                                }
                             }
                         };
                     };
+
+                    // Populate the menu div with all valid menu items
+                    for (var k = 0; k < displayItems.length; k += 1) {
+                        var item = displayItems[k];
+                        $('#menu').append('<div class="row" id="' + k + '"><h3>' +
+                                item.name + '</h3> <br> <h4>' + item.description +
+                                '</h4> <br> <h4>' + item.price + '</h4> </div>');
+                    }
                 });
             }
         });
