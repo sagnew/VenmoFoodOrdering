514617bd2181db7cdcfbe821f2271c9185d4e370
Sam Agnew <sagnew@eden.rutgers.edu>
2014-05-30 17:47:56 -0400

    Fix multiple entry problem for the tray

--- static/js/tray.js
+++ static/js/tray.js
@@ -1,7 +1,35 @@
 // Adds an item to the tray.
 var addToTray = function (item) {
-    // Insert the item into the key, value store.
-    this.items.push(item);
+
+    if (this.items.length > 0) {
+        // Check for duplicate items, and update quantity instead.
+        for (var i = 0; i < this.items.length; i += 1) {
+            if (this.items[i].id === item.id) {
+                var oldItemOptions = this.items[i].options;
+                var newItemOptions = item.options;
+                if (oldItemOptions && newItemOptions) {
+                    // Both items have options.
+                    for (var j = 0; j < oldItemOptions.length; j += 1) {
+                        for (var k = 0; k < newItemOptions.length; k += 1) {
+                            if (oldItemOptions[j] !== newItemOptions[k]) {
+                                // Insert the item into the tray items array.
+                                this.items.push(item);
+                                return;
+                            }
+                        }
+                    }
+                }
+                // These are the same items, therefore just increase quantity.
+                this.items[i].quantity += item.quantity;
+            } else {
+                this.items.push(item);
+                return;
+            }
+        }
+    } else {
+        this.items.push(item);
+        return;
+    }
 
     // Render the tray.
 };
